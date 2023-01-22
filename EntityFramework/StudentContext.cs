052c92346b7aae4c06d500053110a0f5b81a7123
Zalman Izak <thezalmanian@gmail.com>
2023-01-21 23:18:20 -0800

    Created database context class

--- EntityFramework/StudentContext.cs
+++ EntityFramework/StudentContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace EntityFramework
 {
-    internal class StudentContext
+    internal class StudentContext : DbContext
     {
     }
 }
