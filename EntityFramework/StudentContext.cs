bdc48b0614ca35fc1fc0bd45b2f27de672d4a519
Zalman Izak <thezalmanian@gmail.com>
2023-01-21 23:40:31 -0800

    Configured DB Context class to user SQL Server

--- EntityFramework/StudentContext.cs
+++ EntityFramework/StudentContext.cs
@@ -7,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace EntityFramework
 {
-    internal class StudentContext : DbContext
+    public class StudentContext : DbContext
     {
+        public StudentContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Server = The server we are connecting to
+            // Database = The name of the database
+            // Trusted_Connection = Indicates that a windows account should be used
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=EFCoreExample; Trusted_Connection=True;");
+        }
     }
 }
