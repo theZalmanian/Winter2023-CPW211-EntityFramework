7fdc202cda49b8b80534b6ca5cc098402ca7fd28
Zalman Izak <thezalmanian@gmail.com>
2023-01-21 23:45:09 -0800

    Created a student model to track in the database

---
+++ EntityFramework/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    /// <summary>
+    /// Represents an individual college student
+    /// </summary>
+    internal class Student
+    {
+        /// <summary>
+        /// The student's ID number
+        /// </summary>
+        [Key] // Tell EF Core to make this a PK
+        public int StudentID { get; set; }
+
+        /// <summary>
+        /// The student's full legal name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The student's date of birth
+        /// </summary>
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
