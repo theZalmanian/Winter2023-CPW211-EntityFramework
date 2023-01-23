478916303055d79ad22ff9ca3a5becf2117c5543
Zalman Izak <thezalmanian@gmail.com>
2023-01-22 18:50:20 -0800

    Added test code to create a Student and add them to the DB

--- EntityFramework/Program.cs
+++ EntityFramework/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Add Student to DB w/ EF Core
+            using StudentContext dbContext = new();
+
+            // Create a student
+            Student currStudent = new()
+            {
+                FullName = "Reality Undefined",
+                DateOfBirth = new DateTime(1973, 1, 15),
+                EmailAddress = "Reality.UD@email.com"
+            };
+
+            // Prepares the Student insert statement
+            dbContext.Students.Add(currStudent);
+
+            // Execute the pending insert query
+            dbContext.SaveChanges();
         }
     }
 }
